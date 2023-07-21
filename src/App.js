256de623dc58434f87d0a7a967de503dff495770
Bii-teki <geoffrey.bii@student.moringaschool.com>
2023-07-21 13:51:56 +0300

    submit assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] =useState(0)
+  const [text, setText] =useState(0)
+  const [images, setImages] = useState([]);
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = text;
+  }, [text]);
 
+  // useEffect(() => {
+  //   setTimeout(() => setCount(0), 5000);
+  // }, []);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+      .then((r) => r.json())
+      .then((data) => {
+        // setting state in the useEffect callback
+        setImages(data.message);
+      }); 
+    console.log("useEffect called");
+  },[]); 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return<div>
+  <button onClick={(e)=>setCount((count)=> count + 1)}>Click Me {count} times</button>;
+  <input 
+  type="text"
+  placeholder="Type away..."
+  value={text}
+  onChange={(e)=>setText(e.target.value)}
+  
+  />
+  {images.map((image) => (
+        <img src={image} key={image} />
+      ))}
+  
+  
+  </div> 
+
 }
 
 export default App;
